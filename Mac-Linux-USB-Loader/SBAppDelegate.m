a265b5c9cf2a87d63f7f57bcf50adeb768707d18
SevenBits <meokid2@yahoo.com>
2018-11-08 14:15:16 -0500

    More small changes for dark mode

--- Mac-Linux-USB-Loader/SBAppDelegate.m
+++ Mac-Linux-USB-Loader/SBAppDelegate.m
@@ -91,10 +91,6 @@ const NSString *SBBundledEnterpriseVersionNumber = @"0.4.1";
 - (void)setupWelcomeScreenUI {
 	// Make the window background white.
 	self.window.movableByWindowBackground = NO;
-	if ([NSColor respondsToSelector:@selector(underPageBackgroundColor)])
-		self.window.backgroundColor = NSColor.windowBackgroundColor;
-	else
-		self.window.backgroundColor = NSColor.whiteColor;
 
 	// Remove the standard window buttons.
 	[[self.window standardWindowButton:NSWindowZoomButton] setHidden:YES];
@@ -110,6 +106,8 @@ const NSString *SBBundledEnterpriseVersionNumber = @"0.4.1";
 		self.operationsTableView.selectionHighlightStyle = NSTableViewSelectionHighlightStyleSourceList;
 		self.operationsTableView.usesAlternatingRowBackgroundColors = NO;
 		[self.window layoutIfNeeded];
+	} else {
+		self.window.backgroundColor = NSColor.whiteColor;
 	}
 }
 
